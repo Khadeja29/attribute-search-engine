92a164a94b0d006050262aed6ec2533126040031
Khadeja Iqbal <khadejaiqbal@sabanciuniv.edu>
2021-11-30 23:02:56 +0300

    basic file layoutting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import CheckBoxDisplay from './components/checkBoxDisplay.js';
+import NavBar from './components/navBar/navBar.js';
+import SearchBar from './components/searchBar.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+      
+      {/* define a basic structure here first and then call components. */}
+      <NavBar/>
+      <SearchBar/>
+      <CheckBoxDisplay/>
+
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    
+    <div >
+      
+      {/* define a basic structure here first and then call components. */}
+      <h1>Nav bar</h1>
+
+    </div>
+  );
+}
+
+export default NavBar;
