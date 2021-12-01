08f1a4f58766d1754537c36c8969ec6abd633ed0
Khadeja Iqbal <khadejaiqbal@sabanciuniv.edu>
2021-12-01 23:12:06 +0300

    grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
-import CheckBoxDisplay from './components/checkBoxDisplay.js';
 import NavBar from './components/navBar/navBar.js';
-import SearchBar from './components/searchBar.js';
 import './App.css';
 
 function App() {
@@ -9,15 +7,7 @@ function App() {
     
     <div>
        <NavBar/>
-       <div className="container">
-      
-      {/* define a basic structure here first and then call components. */}
-  
-      <SearchBar/>
-      <CheckBoxDisplay/>
-
-
-      </div>
+       
     </div>
    
   );
